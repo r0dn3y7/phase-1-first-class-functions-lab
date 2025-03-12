5b3d6c45eb7c0cb1acbf52bc8e15c60ba15e808d
Rodney <rodney.amani@student.moringaschool.com>
2025-03-12 11:18:16 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+    return array.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(array) {
+    return array.slice(-2);
+  }
+  
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+  
+
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
+  
